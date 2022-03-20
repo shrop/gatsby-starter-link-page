b61f97bf55d7a2eb0dd3328e8f1c932c9982d8ab
Mark Shropshire <mdshrops@shropnet.net>
2022-03-20 19:10:21 -0400

    Added a masonry module and setup a basic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,28 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Masonry from "react-masonry-css"
+
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  700: 2,
+  500: 1
+};
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Link Page</h1>
-    <ul>
-      <li>shrop.dev</li>
-      <li>Twitter</li>
-      <li>Instagram</li>
-      <li>drupal.org</li>
-      <li>Github</li>
-    </ul>
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column">
+      <div>Link 1</div>
+      <div>Link 2</div>
+      <div>Link 3</div>
+      <div>Link 4</div>
+    </Masonry>
   </Layout>
 )
 
